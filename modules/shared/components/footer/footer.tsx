35758b76a23d8fdb77bda3971c37484b5b3f0c15
Frank <frank.pfeift@protonmail.com>
2025-09-25 16:13:18 +0200

    feat: improved footer

--- modules/shared/components/footer/footer.tsx
+++ modules/shared/components/footer/footer.tsx
@@ -1,11 +1,13 @@
+import { PowerhouseLogoIsotype } from '../svgs'
 import { ThemeToggle } from '../theme-toggle'
 
 export function Footer() {
   return (
     <footer className="bg-background/95 supports-[backdrop-filter]:bg-background/60 w-full border-t backdrop-blur">
-      <div className="container flex h-16 items-center justify-center">
-        <div>Vetra - Coming soon</div>
-        <ThemeToggle />
+      <div className="flex h-16 items-center justify-center">
+        <div className="flex items-center">
+          Made with <PowerhouseLogoIsotype className="mx-1 size-4" />
+        </div>
       </div>
     </footer>
   )
